4a05ec0fbd1cc8cf50a96749c168808ef42b7db8
Azharuddin Ali <azharuddinali017@gmail.com>
2025-08-15 18:52:05 +0530

    added validator to name email and number

--- models/itemModel.js
+++ models/itemModel.js
@@ -4,17 +4,22 @@ const itemSchema = new mongoose.Schema(
     {
         name:{
             type: String,
-            required: [true, 'Name is requried']
+            required: [true, 'Name is requried'],
+            trim: true,
+            minlength: [2, 'Name must be at least 2 characters long']
         },
         email:{ 
             type:String,
             requried: [true, 'Email is requried'],
-            unique: true
+            unique: true,
+            match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address']
         },
         phone:{
             type: String,
             requried: [true, 'Phone is requried'],
-            unique: true
+            unique: true,
+            minlength:[10, 'Phone number must be at least 10 digits'],
+            maxlength:[15, 'Phone number must be exceed 15 digits']
         },
         type:{
             type: String,
