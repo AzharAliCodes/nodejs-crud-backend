6294669384e8a901b1dab6fd9c9c86a7067ea735
Azharuddin Ali <azharuddinali017@gmail.com>
2025-08-15 17:37:39 +0530

    Created Item Routes controller

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const {
     createItem,
     getItems,
-    getTtemById,
+    getItemById,
     updateItem,
     deleteItem    
 } = require('../controllers/itemController')
 
-router.post('items',createItem)
+router.post('/items',createItem)
+
+router.get('/items', getItems)
+
+router.get('/items/:id', getItemById)
+
+router.put('/items/:id',updateItem)
+
+router.delete('/items/:id', deleteItem)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const Item = require('./models/itemModel')
+const itemRoutes = require('./routes/itemRoutes')
 require('dotenv').config();
 const app = express();
 
@@ -11,19 +12,21 @@ app.get('/', (req,res) => {
   res.send('server is running')
 })
 
-app.post('/test-add', async (req, res) => {
-  try{
-    const newItem = new Item({
-      name: 'Ali',
-      email:'ali@example.com',
-      phone:'12344567890'
-    });
-    const saved = await newItem.save();
-    res.status(201).json(saved);
-  }catch(err){
-    res.status(400).json({err: err.message})
-  }
-})
+// app.post('/test-add', async (req, res) => {
+//   try{
+//     const newItem = new Item({
+//       name: 'Ali',
+//       email:'ali@example.com',
+//       phone:'12344567890'
+//     });
+//     const saved = await newItem.save();
+//     res.status(201).json(saved);
+//   }catch(err){
+//     res.status(400).json({err: err.message})
+//   }
+// })
+
+
 app.listen(PORT, () => {
     console.log(`Server is Running ${PORT}`)
 });
